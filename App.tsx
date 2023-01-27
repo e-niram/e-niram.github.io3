8d32d3ae768594421b53b6465aeee73112ba505c
e-niram <111471473+e-niram@users.noreply.github.com>
2023-01-26 21:39:27 -0600

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hi, my name is</h1>
+      <p>Esteban Marín</p>
+      <p>I like to solve problems.</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# StackBlitz-PersonalWebsite
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-kulm4p)
